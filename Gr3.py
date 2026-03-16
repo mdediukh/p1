34ced93715cc1adfc0aaf9b2385fbef9ee6d68cf
mdediukh <mdediukh@gmail.com>
2026-03-16 15:10:20 +0100

    2

--- Gr3.py
+++ Gr3.py
@@ -20,7 +20,41 @@ class MainWindow(QtWidgets.QDialog):
     def pobierz(self):
         print('test')
  
+    def zaloguj_uzytkownika(self):
+        self.uzytkownik_imie = self.te_imie.toPlainText().strip()
+        self.uzytkownik_nazwisko = self.te_nazwisko.toPlainText().strip()
+        self.aktualny_uzytkownik = str(self.uzytkownik_imie) + str(self.uzytkownik_nazwisko)
+        print(self.aktualny_uzytkownik)
+
+    def dodaj_ksiazke(self):
+        self_ksiazka_tytul = self.TytulComboBox.currentText().strip()
+        self_ksiazka_autor = self.AutorComboBox.currentText().strip()
+        self_ksiazka_gatunek = self.GatunekComboBox.currentText().strip()
+        self_ksiazka_rok = self.RokComboBox.currentText().strip()
+        self_ksiazka_isbn = self.ISBNComboBox.currentText().strip()
+        if self_ksiazka_tytul == ' ' or self.ksiazka_autor == or self_ksiazka_isbn:
+            QMessageBox.critical(self, "Błąd")
+            return
+
+    def zaloz_baze(self):
+ 
+        self.lista_plikow = ['uzytkownik.txt', 'ksiazki_dostepne.txt', 'ksiazki_wypozyczone.txt']
+        self.LokalizacjaBazy = self.LokalizacjaBazy.toPlainText().strip()
+        self.aktualny_uzytkownik = None
+ 
+        for plik in self.lista_plikow:
+ 
+            plik = self.LokalizacjaBazy + '\\' + plik
+ 
+            if os.path.exists(plik):
+                print(f'Plik_istnieje: {plik}')
+               
+            elif not os.path.exists(plik):
+                with open(plik, 'w') as zapis:
+                    print(f'Tworze: {plik}')
+
     def zaloz_uzytkownika(self):
+        self.LokalizacjaBazy = self.te_konfig__baza.toPlainText().strip()
         plik_uzytkownika = self.LokalizacjaBazy + '\\' + self.lista_plikow[0]
         if os.path.exists(plik_uzytkownika):
             print(plik_uzytkownika)
@@ -38,21 +72,7 @@ class MainWindow(QtWidgets.QDialog):
                         else: 
                             odczyt_uzytkownik.write(str(self.u_imie) + '#' +str(self.u_imie)+ '#' + str(self.uid_uzytkownik+ '\n'))
 
-    def zaloz_baze(self):
- 
-        self.lista_plikow = ['uzytkownik.txt', 'ksiazki_dostepne.txt', 'ksiazki_wypozyczone.txt']
-        self.LokalizacjaBazy = self.LokalizacjaBazy.toPlainText().strip()
- 
-        for plik in self.lista_plikow:
- 
-            plik = self.LokalizacjaBazy + '\\' + plik
- 
-            if os.path.exists(plik):
-                print(f'Plik_istnieje: {plik}')
-               
-            elif not os.path.exists(plik):
-                with open(plik, 'w') as zapis:
-                    print(f'Tworze: {plik}')
+
  
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
